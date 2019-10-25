198bf8cc8dc917230b76fe8c29d87a571703e388
DrAl-HFS <DrAl.HFS@gmail.com>
2019-10-25 12:16:42 +0100

    Refactored for GPU: still non functional.

--- src/binMapACC.c
+++ src/binMapACC.c
@@ -34,27 +34,31 @@ ACC_INLINE int sgnIdxDiff (const MKFAccScalar f, const MKFAccScalar t[BM_NUMT])
    return( 1 - (f < t[BM_TLB])) + (f > t[BM_TUB]); // 1+sgnF32(f-t)
 } // sgnIdxDiff
 
-ACC_INLINE int binMap (const MKFAccScalar f, const MKFAccBinMap * const pC)
+ACC_INLINE int binMap (const MKFAccScalar f, const MKFAccBinMap * const pM)
 {  // pragma acc data present( pC[:1] )
-   return( (pC->m >> sgnIdxDiff(f, pC->t) ) & 0x1 );
+   return( (pM->m >> sgnIdxDiff(f, pM->t) ) & 0x1 );
 } // binMap
+
+#pragma acc routine vector
+ACC_INLINE BMPackWord binMapW (const MKFAccScalar * restrict pF, const MKFAccBinMap *pM)
+{
+   BMPackWord w= 0;
+#if 0
+   BMPackWord v[32]; // for vectorisation
+   #pragma acc loop vector
+   for (int j= 0; j < 32; j++) { v[j]= binMap(pF[j], pM) << j; }
+
+   #pragma acc loop reduction(+: w )
+   for (int j= 0; j < 32; j++) { w += v[j]; }
+#else
+   #pragma acc loop reduction(+: w )
+   for (int j= 0; j < 32; j++) { w += binMap(pF[j], pM) << j; }
+#endif
+   return(w);
+} // binMapW
 
 
 /***/
-
-// For testing - do not use
-#pragma acc routine vector
-void binMapNF32G (BMPackWord * restrict pW, const MKFAccScalar * restrict pF, const size_t n, const MKFAccBinMap * const pM)
-{
-   #pragma acc data present( pW[:n], pF[:n], pM[:1] )
-   {
-      #pragma acc loop vector
-      for (size_t i= 0; i < n; i++)
-      { //
-         pW[i]= binMap(pF[i], pM) << (i & 0x1F);
-      }
-   }
-} // binMapNF32G
 
 #pragma acc routine vector
 void binMapAcc (BMPackWord * restrict pW, const MKFAccScalar * restrict pF, const size_t nF, const MKFAccBinMap *pM)
@@ -62,22 +66,10 @@ void binMapAcc (BMPackWord * restrict pW, const MKFAccScalar * restrict pF, cons
    const size_t nW= nF>>5; // NB: truncates non multiple of 32!
    #pragma acc data present( pW[:nW], pF[:nF], pM[:1] )
    {
-      //U32 v[32], w; //parallel loop
-      #pragma acc loop vector// private(v,w)
+      #pragma acc loop vector
       for (size_t i= 0; i < nW; i++)
       {
-#if 0
-         #pragma acc loop vector
-         for (int j= 0; j < 32; j++) { v[j]= binMap(pF[(i*32)+j], pM) << j; }
-
-         #pragma acc loop reduction(+: w )
-         for (int j= 0; j < 32; j++) { w += v[j]; }
-#else
-         U32 w= 0;
-         #pragma acc loop reduction(+: w )
-         for (int j= 0; j < 32; j++) { w += binMap(pF[(i*32)+j], pM) << j; }
-#endif
-         pW[i]= w;
+         pW[i]= binMapW(pF+i*32, pM);
       }
    }
 } // binMapAcc
@@ -92,7 +84,7 @@ void binMapRowsAcc
    #pragma acc data present( pW[:rowStrideBM*nRows], pF[:rowLenF*nRows], pM[:1] )
    {
       const int rowLenBits= (rowLenF + 0x1F) & ~0x1F;
-      #pragma acc parallel
+      #pragma acc parallel loop
       for (int i= 0; i < nRows; i++)
       {
          binMapAcc(pW + i * rowStrideBM, pF + i * rowLenF, rowLenBits, pM);
